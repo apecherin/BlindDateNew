7a75b59a0b29ceac84519144cdac4513e7f513c6
apecherin <artempecherin@gmail.com>
2014-07-25 00:03:15 +0400

    remove idea

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Product.create("age" => "20",
+User.create("age" => "20",
     "avatar" => "",
     "avatar_content_type" => "image/jpeg",
     "avatar_file_name" => "images.jpeg",
